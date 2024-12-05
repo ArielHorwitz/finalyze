cc551401df656d417fd6ce9494bf30b5f2b0e11b
Ariel Horwitz <ArielHorwitz@users.noreply.github.com>
2024-12-05 22:42:13 +0200

    Import arbitrary source data file

--- finalyze/source/__init__.py
+++ finalyze/source/__init__.py
@@ -1,9 +1,8 @@
-import math
 from pathlib import Path
 
-import pandas as pd
 import polars as pl
 
+from finalyze.source.leumi import parse_file
 from finalyze.utils import flip_rtl_column, print_table
 
 SOURCE_SCHEMA = {
@@ -19,61 +18,28 @@ def add_subparser(subparsers):
     parser = subparsers.add_parser("source", help="Import source data")
     parser.set_defaults(func=run)
     parser.add_argument(
+        "files",
+        nargs="+",
+        help="Account balance and credit card .xls files exported from Bank Leumi",
+    )
+    parser.add_argument(
         "-n",
         "--account-name",
         help="Name of account",
-    )
-    parser.add_argument(
-        "-a",
-        "--account-files",
-        nargs="*",
-        default=[],
-        help="Account balance .xls files exported from Bank Leumi",
-    )
-    parser.add_argument(
-        "-c",
-        "--credit-files",
-        nargs="*",
-        default=[],
-        help="Credit card .xls files exported from Bank Leumi",
-    )
-    parser.add_argument(
-        "-A",
-        "--account-files-dir",
-        help="Directory of account balance files",
-    )
-    parser.add_argument(
-        "-C",
-        "--credit-files-dir",
-        help="Directory of credit card files",
     )
 
 
 def run(args):
     verbose = args.verbose
-    account_files = [Path(f).resolve() for f in args.account_files]
-    credit_files = [Path(f).resolve() for f in args.credit_files]
+    files = [Path(f).resolve() for f in args.files]
     account_name = args.account_name or args.dataset_name
-    if args.account_files_dir is not None:
-        account_files.extend(Path(args.account_files_dir).glob("*.xls"))
-    if args.credit_files_dir is not None:
-        credit_files.extend(Path(args.credit_files_dir).glob("*.xls"))
     output_file = args.source_dir / f"{account_name}.csv"
     if verbose:
-        print("Account files:")
-        for f in sorted(account_files):
+        print("Source files:")
+        for f in sorted(files):
             print(f"  {f}")
-        print("Credit files:")
-        for f in sorted(credit_files):
-            print(f"  {f}")
-    if len(account_files) + len(credit_files) == 0:
-        raise RuntimeError("No source files provided")
-    parsed_data = parse_sources(
-        account_name=account_name,
-        balance_files=account_files,
-        credit_files=credit_files,
-        verbose=verbose,
-    )
+    parsed_data = parse_sources(files=files, account_name=account_name)
+    print_table(parsed_data, "Parsed data", verbose)
     print(f"Writing output to: {output_file}")
     output_file.parent.mkdir(parents=True, exist_ok=True)
     parsed_data.write_csv(output_file)
@@ -89,96 +55,13 @@ def get_source_data(args):
     return source_data
 
 
-def parse_sources(*, account_name, balance_files, credit_files, verbose):
-    balance_dfs = [
-        parse_balance(input_file=file, verbose=verbose) for file in balance_files
-    ]
-    credit_dfs = [
-        parse_credit(input_file=file, verbose=verbose) for file in credit_files
-    ]
-    if balance_dfs:
-        print_table(pl.concat(balance_dfs), "Balance", verbose > 1)
-    if credit_dfs:
-        print_table(pl.concat(credit_dfs), "Credit", verbose > 1)
+def parse_sources(*, files, account_name):
+    raw_dfs = [parse_file(input_file=file) for file in files]
     final = (
-        pl.concat(balance_dfs + credit_dfs)
+        pl.concat(raw_dfs)
+        .with_columns(pl.lit(account_name).alias("account"))
         .unique()
         .sort("date", "amount")
-        .with_columns(pl.lit(account_name).alias("account"))
         .select(SOURCE_SCHEMA.keys())
     )
-    print_table(final, "Parsed data", verbose)
     return final
-
-
-def parse_credit(*, input_file, verbose=False):
-    def inner_parse(raw_pd_df, source_name):
-        print_table(raw_pd_df, f"raw {source_name}", verbose > 1)
-        validate(raw_pd_df)
-        table = pl.from_pandas(raw_pd_df.iloc[2:-1])
-        parsed = pl.DataFrame(
-            {
-                "source": source_name,
-                "date": table.select(
-                    pl.col("0").str.strptime(pl.Date, format="%d/%m/%y")
-                ),
-                "amount": table.select(pl.col("5").cast(pl.Float64).mul(-1)),
-                "description": table.select(pl.col("1").cast(pl.String)),
-                # "notes": table.select(pl.col("4").cast(pl.String)),
-            }
-        )
-        print_table(parsed.sort("date"), f"parsed {source_name}", verbose > 1)
-        return parsed
-
-    def validate(raw_pd_df):
-        try:
-            assert raw_pd_df.iloc[0, 0] in (
-                'עסקאות מחויבות בש"ח (לידיעה בלבד)',
-                'עסקאות בש"ח במועד החיוב',
-            )
-            assert raw_pd_df.iloc[1, 0] == "תאריך העסקה"
-            assert raw_pd_df.iloc[-1, 4] == 'סה"כ:'
-            assert math.isnan(raw_pd_df.iloc[-1, 0])
-        except AssertionError:
-            raise ImportError(f"Unexpected format for {input_file}")
-
-    # parse parts
-    raw_html = pd.read_html(input_file, encoding="utf-8")
-    raw_tables = [inner_parse(raw_html[2], "Credit card")]
-    if len(raw_html) >= 4:
-        debit_table = inner_parse(raw_html[3], "Debit")
-        raw_tables.append(debit_table)
-
-    # combine
-    parsed = pl.concat(raw_tables)
-    print_table(parsed, "parsed credit", verbose > 1)
-    return parsed
-
-
-def parse_balance(*, input_file, verbose=False):
-    balances_raw_pd = pd.read_html(input_file, encoding="utf-8")[2]
-    balances_raw_pd[0] = balances_raw_pd[0].str.replace("** ", "", regex=False)
-    print_table(balances_raw_pd, "raw balance", verbose > 1)
-    try:
-        assert balances_raw_pd.iloc[0, 0] == "תנועות בחשבון"
-        assert balances_raw_pd.iloc[1, 0] == "תאריך"
-    except AssertionError:
-        raise ImportError(f"Unexpected format for {input_file}")
-    if balances_raw_pd.iloc[-1, 0].startswith("**"):
-        balances_raw_pd = balances_raw_pd.iloc[:-1]
-    raw = pl.from_pandas(balances_raw_pd.iloc[2:])
-    data = {
-        "source": "Account txn",
-        "date": raw.select(pl.col("0").str.strptime(pl.Date, format="%d/%m/%y")),
-        "amount": raw.select(
-            pl.col("5").cast(pl.Float64) - pl.col("4").cast(pl.Float64)
-        ),
-        "description": raw.select(pl.col("2").cast(pl.String)),
-        # "notes": raw.select(pl.col("7").cast(pl.String)),
-    }
-    parsed = pl.DataFrame(data)
-    print_table(parsed.sort("date"), "parsed balance", verbose > 1)
-    # remove direct credit card transactions
-    filtered = parsed.filter(pl.col("description") != "לאומי ויזה")
-    print_table(filtered.sort("date"), "filtered balance", verbose > 1)
-    return filtered
---
+++ finalyze/source/leumi.py
@@ -0,0 +1,126 @@
+import math
+
+import pandas as pd
+import polars as pl
+
+
+def parse_file(input_file):
+    try:
+        return BalanceFormat.parse(input_file)
+    except ImportError as exc:
+        balance_error = UnexpectedFormat(f"Unexpected file type ({exc!r})")
+    except (UnexpectedFormat, ImportError) as exc:
+        balance_error = exc
+    try:
+        return CreditFormat.parse(input_file)
+    except ImportError as exc:
+        credit_error = UnexpectedFormat(f"Unexpected file type ({exc!r})")
+    except UnexpectedFormat as exc:
+        credit_error = exc
+    raise UnexpectedFormat(
+        f"{input_file} not balance: {balance_error!r}, not credit: {credit_error!r}"
+    )
+
+
+class UnexpectedFormat(Exception):
+    """Raised when encountering an unexpected format."""
+
+
+class BalanceFormat:
+    EXPECTED_HEADER = "תנועות בחשבון"
+    EXPECTED_FIRST_COLUMN_NAME = "תאריך"
+    CREDIT_CARD_DESCRIPTION = "לאומי ויזה"
+
+    @classmethod
+    def check(cls, raw_df):
+        if (header := raw_df.iloc[0, 0]) != cls.EXPECTED_HEADER:
+            raise UnexpectedFormat(
+                f"Expected header {cls.EXPECTED_HEADER!r}, got: {header!r}"
+            )
+        if (first_column := raw_df.iloc[1, 0]) != cls.EXPECTED_FIRST_COLUMN_NAME:
+            raise UnexpectedFormat(
+                f"Expected column name {cls.EXPECTED_FIRST_COLUMN_NAME!r}"
+                f", got: {first_column!r}"
+            )
+
+    @classmethod
+    def parse(cls, input_file):
+        raw_html = pd.read_html(input_file, encoding="utf-8")
+        raw_df = raw_html[2]
+        # Remove leading asterisks from otherwise valid data (rows from today)
+        raw_df[0] = raw_df[0].str.replace("** ", "", regex=False)
+        cls.check(raw_df)
+        if raw_df.iloc[-1, 0].startswith("**"):
+            raw_df = raw_df.iloc[:-1]
+        raw = pl.from_pandas(raw_df.iloc[2:])
+        data = {
+            "date": raw.select(pl.col("0").str.strptime(pl.Date, format="%d/%m/%y")),
+            "amount": raw.select(
+                pl.col("5").cast(pl.Float64) - pl.col("4").cast(pl.Float64)
+            ),
+            "description": raw.select(pl.col("2").cast(pl.String)),
+            # "notes": raw.select(pl.col("7").cast(pl.String)),
+        }
+        parsed = pl.DataFrame(data).with_columns(pl.lit("Account txn").alias("source"))
+        # remove direct credit card transactions
+        filtered = parsed.filter(pl.col("description") != cls.CREDIT_CARD_DESCRIPTION)
+        return filtered
+
+
+class CreditFormat:
+    EXPECTED_HEADERS = (
+        'עסקאות מחויבות בש"ח (לידיעה בלבד)',
+        'עסקאות בש"ח במועד החיוב',
+    )
+    EXPECTED_FIRST_COLUMN_NAME = "תאריך העסקה"
+    EXPECTED_LAST_ROW_VALUE = 'סה"כ:'
+
+    @classmethod
+    def check(cls, raw_df):
+        if (header := raw_df.iloc[0, 0]) not in cls.EXPECTED_HEADERS:
+            raise UnexpectedFormat(
+                f"Expected header {cls.EXPECTED_HEADERS!r}" f", got: {header!r}"
+            )
+        if (first_column := raw_df.iloc[1, 0]) != cls.EXPECTED_FIRST_COLUMN_NAME:
+            raise UnexpectedFormat(
+                f"Expected column name {cls.EXPECTED_FIRST_COLUMN_NAME!r}"
+                f", got: {first_column!r}"
+            )
+        if (last_row_value := raw_df.iloc[-1, 4]) != cls.EXPECTED_LAST_ROW_VALUE:
+            raise UnexpectedFormat(
+                f"Expected {cls.EXPECTED_LAST_ROW_VALUE!r} in last row"
+                f", got: {last_row_value!r}"
+            )
+        if not math.isnan(expected_nan := raw_df.iloc[-1, 0]):
+            raise UnexpectedFormat(f"Expected nan, got: {expected_nan!r}")
+
+    @classmethod
+    def parse(cls, input_file):
+        raw_html = pd.read_html(input_file, encoding="utf-8")
+        raw_tables = [
+            cls._table_parse(raw_html[2]).with_columns(
+                pl.lit("Credit card").alias("source")
+            )
+        ]
+        if len(raw_html) >= 4:
+            debit_table = cls._table_parse(raw_html[3]).with_columns(
+                pl.lit("Debit").alias("source")
+            )
+            raw_tables.append(debit_table)
+        return pl.concat(raw_tables)
+
+    @classmethod
+    def _table_parse(cls, raw_df):
+        cls.check(raw_df)
+        table = pl.from_pandas(raw_df.iloc[2:-1])
+        parsed = pl.DataFrame(
+            {
+                "date": table.select(
+                    pl.col("0").str.strptime(pl.Date, format="%d/%m/%y")
+                ),
+                "amount": table.select(pl.col("5").cast(pl.Float64).mul(-1)),
+                "description": table.select(pl.col("1").cast(pl.String)),
+                # "notes": table.select(pl.col("4").cast(pl.String)),
+            }
+        )
+        return parsed
