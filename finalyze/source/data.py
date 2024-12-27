13c3a19498ba5f51c25d7030beee8b2d46e0be17
Ariel Horwitz <ArielHorwitz@users.noreply.github.com>
2024-12-27 13:33:21 +0200

    Fix sorting source data

--- finalyze/source/data.py
+++ finalyze/source/data.py
@@ -28,7 +28,7 @@ ENRICHED_SCHEMA = {
 def load_source_data(source_dir):
     return pl.concat(
         pl.read_csv(file, schema=RAW_SCHEMA) for file in source_dir.glob("*.csv")
-    )
+    ).sort("date", "amount")
 
 
 def enrich_source(source):
