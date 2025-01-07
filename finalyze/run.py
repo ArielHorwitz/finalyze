02306bff48c297d215462f5b6c73148249a9cba4
Ariel Horwitz <ArielHorwitz@users.noreply.github.com>
2025-01-07 19:28:39 +0200

    Run entire pipeline by default

--- finalyze/run.py
+++ finalyze/run.py
@@ -18,18 +18,26 @@ def add_developer_subcommand(subparsers):
 
 def parse_args():
     parser = argparse.ArgumentParser(prog=APP_NAME, description=APP_DESCRIPTION)
-    parser.set_defaults(run=run)
+    parser.set_defaults(run=run_pipeline)
     parser.add_argument(
         "--config-file",
-        help="Location of config file",
+        help=f"Location of config file [default: {CONFIG_FILE}]",
         default=str(CONFIG_FILE),
     )
     # Subcommands
-    subparsers = parser.add_subparsers(dest="subcommand")
+    subparsers = parser.add_subparsers(
+        dest="subcommand",
+        title="subcommands",
+        description="Run a specific subcommand of the pipeline",
+        help="Pipeline subcommand",
+        metavar="[SUBCOMMAND]",
+    )
     subparsers.add_parser("ingest", help="Ingest source data").set_defaults(
         run=ingest.run
     )
-    subparsers.add_parser("tag", help="Tag source data").set_defaults(run=tag.run)
+    subparsers.add_parser("tag", help="Categorize transactions").set_defaults(
+        run=tag.run
+    )
     subparsers.add_parser("analyze", help="Analyze historical data").set_defaults(
         run=analyze.run
     )
@@ -39,8 +47,11 @@ def parse_args():
     return args
 
 
-def run(config):
+def run_pipeline(config):
     pprint.pprint(config.model_dump())
+    ingest.run(config)
+    tag.run(config)
+    analyze.run(config)
 
 
 def main():
